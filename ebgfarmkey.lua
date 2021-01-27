302bd3d882db5ac7f6e5fb0f189bccc7e1725880
scrypt0r <75549090+scrypt0r@users.noreply.github.com>
2021-01-27 18:37:26 -0500

    Update ebgfarmkey.lua

--- ebgfarmkey.lua
+++ ebgfarmkey.lua
@@ -11,9 +11,9 @@ local whitelist = {
     "1B463505-4D5F-44CD-A2C6-B94B3E99801D", -- log's accounts
     "E713B213-78BB-4826-AEA6-F19F9AD2AEEB", -- brian's accounts
     "48287182-9E77-4F97-A654-97069ED8166C", -- jess's accounts
-    -- "6DA4CA6A-F7E3-4D9E-B9CA-295E24CF8EED" -- john's accounts
-    "2025A77F-AABC-4214-9CC3-73F06C459979" -- john's accounts
-    "837736AA-0192-47F9-98C5-8DD0DF18FE74" -- sterling's accounts
+    -- "6DA4CA6A-F7E3-4D9E-B9CA-295E24CF8EED", -- john's accounts
+    "2025A77F-AABC-4214-9CC3-73F06C459979", -- john's accounts
+    "837736AA-0192-47F9-98C5-8DD0DF18FE74", -- sterling's accounts
     "4EE2B08D-D507-4403-98A0-23495B58A171" -- vastorio's accounts
 }
 
