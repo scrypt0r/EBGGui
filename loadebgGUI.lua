accb7bd96ee493ff72ada7f4be655e802c9ed8c9
scrypt0r <75549090+scrypt0r@users.noreply.github.com>
2021-01-27 18:42:54 -0500

    Update loadebgGUI.lua

--- loadebgGUI.lua
+++ loadebgGUI.lua
@@ -21,6 +21,6 @@ if ISGAME == true then
     whitelisturl = "https://raw.githubusercontent.com/scrypt0r/EBGGui/main/ebgGUIkey.lua"
     guiurl = "https://raw.githubusercontent.com/scrypt0r/EBGGui/main/ebgGUI.lua"
     loadstring(game:HttpGet(whitelisturl, true))();
-    wait(0.5)
+    wait()
     loadstring(game:HttpGet(guiurl, true))();
 end
